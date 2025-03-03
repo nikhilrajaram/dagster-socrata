cb7ce25fc31e210f581a2630abf2b15c2d5487a4
Ben Galewsky <ben@peartreestudio.net>
2025-03-03 16:32:59 -0600

    Bundle up job to download from socrata

--- dagster_socrata/definitions.py
+++ dagster_socrata/definitions.py
@@ -1,13 +1,20 @@
-from dagster import Definitions, load_assets_from_modules, EnvVar
+from dagster import Definitions, load_assets_from_modules, EnvVar, define_asset_job
 from dagster_aws.s3 import S3Resource
 
 from dagster_socrata import assets  # noqa: TID252
 from dagster_socrata.resource import SocrataResource
 
 all_assets = load_assets_from_modules([assets])
+socrata_job = define_asset_job(
+    name="DownloadSocrataDataset",
+    selection=[assets.socrata_metadata,
+               assets.socrata_to_object_store,
+               assets.socrata_to_deltalake]
+)
 
 defs = Definitions(
     assets=all_assets,
+    jobs=[socrata_job],
     resources={
         "socrata": SocrataResource(
             domain=EnvVar("SOCRATA_DOMAIN"),
